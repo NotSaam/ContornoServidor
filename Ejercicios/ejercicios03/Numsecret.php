1d23d7d40b31c06241ed2fddc32e30f53860efee
Sam <samuel.fuentes.baio@gmail.com>
2023-11-03 14:05:39 +0100

    numsecreto

--- Ejercicios/ejercicios03/Numsecret.php
+++ Ejercicios/ejercicios03/Numsecret.php
@@ -5,7 +5,34 @@
 </head>
 <body>
     <h1>Adivina el número secreto</h1>
-    <?php 
+    <?php
+
+    $aleatorio = 0;
+    $intentos = 0;
+
+    if (isset($_POST["aleatorio"])) {
+        $aleatorio = $_POST["aleatorio"];
+    }
+
+    if (isset($_POST["intentos"])) {
+        $intentos = $_POST["intentos"];
+    }
+
+    if (isset($_POST["numero"])) {
+        $n = $_POST["numero"];
+        $intentos++;
+        echo "Tu número es: $n<br>";
+
+        if ($n > $aleatorio) {
+            echo "Tu número es menor.";
+        } elseif ($n < $aleatorio) {
+            echo "Tu número es mayor.";
+        } else {
+            echo "¡Enhorabuena, adivinaste! Solo te tomó $intentos intentos.";
+        }
+        // En lugar de generar un nuevo número aleatorio, mantenemos el mismo valor
+        echo "<a href=numsecretform.php?aleatorio=$aleatorio&intentos=$intentos>Volver</a>";
+    }
     ?>
 </body>
 </html>
--- Ejercicios/ejercicios03/Numsecretform.php
+++ Ejercicios/ejercicios03/Numsecretform.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Numero secreto</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    </head>
-    <body>
+<head>
+    <title>Numero secreto</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
     <h1>Adivina o numero secreto</h1>
 
-<form method="post" action="calculadora.php">
-    <label for="numero">Numero:</label>
-    <input type="number" name="numero" id="numero" required>
-    <input type="hidden" name="numero" id="numero"  value>
-    <input type="submit" value="Enviar">
-</form>
-
-
-    </body>
+    <form method="POST" action="numsecret.php">
+        <label for="numero">Numero:</label>
+        <input type="number" name="numero" id="numero" required>
+        <?php
+        $aleatorio = isset($_REQUEST["aleatorio"]) ? $_REQUEST["aleatorio"] : rand(1, 100);
+        $intentos = isset($_REQUEST["intentos"]) ? $_REQUEST["intentos"] : 0;
+        ?>
+        <input type="hidden" name="aleatorio" value="<?php echo $aleatorio; ?>">
+        <input type="hidden" name="intentos" value="<?php echo $intentos; ?>">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
 </html>

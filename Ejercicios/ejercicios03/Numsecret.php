f0dce66d15eeef451f6feb35efbfd751e1d2078c
sam <samuel.fuentes.baio@gmail.com>
2023-11-03 16:45:37 +0100

    a

---
+++ Ejercicios/ejercicios03/Numsecret.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Adivina el número secreto</title>
+</head>
+<body>
+    <h1>Adivina el número secreto</h1>
+    <?php
+
+    $aleatorio = 0;
+    $intentos = 0;
+
+    if (isset($_POST["aleatorio"])) {
+        $aleatorio = $_POST["aleatorio"];
+    }
+
+    if (isset($_POST["intentos"])) {
+        $intentos = $_POST["intentos"];
+    }
+
+    if (isset($_POST["numero"])) {
+        $n = $_POST["numero"];
+        $intentos++;
+        echo "Tu número es: $n<br>";
+
+        if ($n > $aleatorio) {
+            echo "Tu número es menor.";
+        } elseif ($n < $aleatorio) {
+            echo "Tu número es mayor.";
+        } else {
+            echo "¡Enhorabuena, adivinaste! Solo te tomó $intentos intentos.";
+        }
+        // En lugar de generar un nuevo número aleatorio, mantenemos el mismo valor
+        echo "<a href=numsecretform.php?aleatorio=$aleatorio&intentos=$intentos>Volver</a>";
+    }
+    ?>
+</body>
+</html>
--- Ejercicios/ejercicios03/Numsecretform.php
+++ Ejercicios/ejercicios03/Numsecretform.php
@@ -7,18 +7,6 @@
 <body>
     <h1>Adivina o numero secreto</h1>
 
-<<<<<<< HEAD
-<form method="post" action="numsecret.php">
-    <label for="numero">Numero:</label>
-    <input type="number" name="numero" id="numero" required>
-    <input type="hidden" name="numero" id="numero" required>
-    <input type="submit" value="Adivinar">
-</form>
-
-
-    </body>
-</html>
-=======
     <form method="POST" action="numsecret.php">
         <label for="numero">Numero:</label>
         <input type="number" name="numero" id="numero" required>
@@ -32,4 +20,3 @@
     </form>
 </body>
 </html>
->>>>>>> cd01e206272d3ed5274facec14604300f79abce3

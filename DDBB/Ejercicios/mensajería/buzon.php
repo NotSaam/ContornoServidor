e54d6e2efdbd14efc57d037394119a97f0085153
Sam <samuel.fuentes.baio@gmail.com>
2023-11-29 14:25:21 +0100

    mensajeria

---
+++ DDBB/Ejercicios/mensajería/buzon.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Buzon</title>
+    <link rel="stylesheet" href="../css/mens.css">
+
+</head>
+
+<body>
+
+    <h1>Hola <!-- <usuario --></h1>
+
+    <div class="container">
+        <p>Lista de mensajes</p>
+
+
+        <p>Nuevo mensaje</p>
+        <input id="input-buzon" type="text"  placeholder="Destinatario">
+        <textarea name="textarea" rows="10" cols="50"></textarea>
+
+            <button type="submit">Enviar mensaje</button>
+        </form>
+    </div>
+
+</body>
+</html>

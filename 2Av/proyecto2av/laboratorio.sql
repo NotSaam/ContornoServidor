699ea8e3c66643cb85995c100b9f24b4c0882b93
Sam <samuel.fuentes.baio@gmail.com>
2024-01-19 10:22:46 +0100

    db laboratorio

--- 2Av/proyecto2av/laboratorio.sql
+++ 2Av/proyecto2av/laboratorio.sql
@@ -33,7 +33,7 @@ INSERT INTO `categorias` (`CodCat`, `Nombre`, `Descripcion`) VALUES
 	(2, 'ProtFija', 'Prótesis fija ' ),
 	(3, 'Compost', 'Composturas' );
 	(4, 'ProtRemov', 'Prótesis Removibles' );
-	(5, 'FerMich', 'Férulas tipo Michigan' );
+	(5, 'FerMichi', 'Férulas tipo Michigan' );
 /*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
 
 -- Volcando estructura para tabla laboratorio.pedidos
@@ -95,13 +95,27 @@ CREATE TABLE IF NOT EXISTS `productos` (
 
 -- Volcando datos para la tabla laboratorio.productos: ~6 rows (aproximadamente)
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` (`CodProd`, `Nombre`, `Descripcion`, `Peso`, `Stock`, `CodCat`,`Estado`, `Precio`) VALUES
-	(1, 'Harina', '8 paquetes de 1kg de harina cada uno', 8, 100, 1,true),
-	(2, '', '', , , ,''),
-	(3, '', '', , , ,''),
-	(4, '', '', , , ,''),
-	(5, '', '', , , , ''),
-	(6, '', ' ', , , ,'');
+INSERT INTO `productos` (`CodProd`, `Nombre`, `Peso`, `Stock`, `CodCat`,`Estado`, `Precio`) VALUES
+	(1, 'Corona individual', 8, 100, 1,true,15.4)
+	(2, 'Restauracion multiple atronillada y cementada',10 ,200 ,1 ,true,30),
+	(3, 'Hibridas en resina sobre estructura colada y fresada',25,100 ,1,true ,27),
+	(4, 'Restauraciones totales atornilladas de ceramica sobre metal',12 ,100 ,1,true,40),
+	(5, 'Restauraciones totales sobre base de oxido de zirconio fresado',48, 100,1,true,35),
+	(6, 'Protesis provisionales ',4 ,100 ,2 ,true,50.4);
+	(7, 'Coronas individuales definitivas en: gradia(composite ultima generacion)', 14,100 ,2 ,true,12);
+	(8, 'Coronas con base de óxido de zicornio y recubrimiento cerámico',9.23 ,100 ,2 ,true,24.15);
+	(9, 'Coronas ceramo-metálicas', 7.26,100 ,2 ,true,25);
+	(10, 'Coronas individuales en cerámica inyectada, de gran efecto estético', 5.67,100 ,2 ,true,20.5);
+	(11, 'Restauraciones múltiples: desde puentes de 2 piezas hasta rehabilitaciones de 14,tanto sobre estructuras de óxido de zirconio como cerámica sobre metal', 6,100 ,2 ,true,55);
+  (12,'Reparaciones de protesis acrilicas', , , , , ,)
+  (13,'Soldaduras de estructuras de cromo cobalto', , , , , ,)
+  (14,'Reposicion de piezas en protesis de resina y esqueleticas', , , , , ,)
+  (15,'Agregado de piezas y ganchos en todo tipo de protesis', , , , , ,)
+  (16,'Rebase de todo tipo de protesis', , , , , ,)
+  (17,'En resina (completa y parciales)', , , , , ,)
+  (18,'Con base metalica (esqueléticos con base de cromo-cobalto) ', , , , , ,)
+  (19,'Combinada: con ataches en combinacion con elementos fijos', , , , , ,)
+  (20,'En resina acrilica', , , , , ,)
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 
 -- Volcando estructura para tabla laboratorio.usuarios

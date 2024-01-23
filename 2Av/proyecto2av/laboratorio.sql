1d5f19ddca02c3a5c1ea3e3264e8d129f5e27bf6
Sam <samuel.fuentes.baio@gmail.com>
2024-01-23 10:10:43 +0100

    db

--- 2Av/proyecto2av/laboratorio.sql
+++ 2Av/proyecto2av/laboratorio.sql
@@ -1,73 +1,35 @@
 CREATE DATABASE if NOT EXISTS 'laboratorio'
 USE 'laboratorio';
 
-CREATE TABLE if NOT exists `categorias` (
-	`Cod_Cat` INT(11) NOT NULL AUTO_INCREMENT,
-	`Nombre` VARCHAR(50) NOT NULL,
-	`Descripcion` VARCHAR(200) NOT NULL,
-	`Activa` boolean NOT NULL,
-	PRIMARY KEY (`Cod_Cat`) 
-	)
-INSERT INTO `categorias` (`Cod_Cat`, `Nombre`, `Descripcion`,`Activa`) VALUES
-(1,'ProtImpl', 'Protesis sobre implantes',true),
-(2,'ProtFija', 'Prótesis fija ',true),
-(3,'Compost', 'Composturas' , true), 
-(4,'ProtRemov', 'Prótesis Removibles',true),
-(5,'FerMichi', 'Férulas tipo Michigan' ,true);
+-- Tabla "categorias"
+CREATE TABLE categorias (
+    Cod_Cat INT PRIMARY KEY,
+    Nombre VARCHAR(255),
+    Descripcion VARCHAR(255),
+    Activa BOOLEAN
+  );
 
+  INSERT INTO `categorias` (`Cod_Cat`, `Nombre`, `Descripcion`,`Activa`) VALUES
+  (1,'ProtImpl', 'Protesis sobre implantes',true),
+  (2,'ProtFija', 'Prótesis fija ',true),
+  (3,'Compost', 'Composturas' , true), 
+  (4,'ProtRemov', 'Prótesis Removibles',true),
+  (5,'FerMichi', 'Férulas tipo Michigan' ,true);
 
+-- Tabla "productos"
+CREATE TABLE productos (
+    Cod_Producto INT PRIMARY KEY,
+    Nombre VARCHAR(255),
+    Info VARCHAR(255),
+    Peso DECIMAL(10, 2),
+    Stock INT,
+    Activo BOOLEAN,
+    Costo DECIMAL(10, 2),
+    Cod_Categoria INT,
+    FOREIGN KEY (Cod_Categoria) REFERENCES categorias(Cod_Cat)
+  );
 
-CREATE TABLE IF NOT EXISTS `pedidos` (
-  `Cod_Pedido` int(11) NOT NULL AUTO_INCREMENT,
-  `Cod_User` int(11) NOT NULL,
-  `Fecha` datetime NOT NULL,
-  `Precio_total` float NOT NULL,
-  `Cod_Estado` int(11) NOT NULL,
-  PRIMARY KEY (`CodPed`),
-  KEY `CodUser` (`Cod_User`),
-  CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`Cod_User`) REFERENCES `usuarios` (`Cod_User`)
-  ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-
-  INSERT INTO `pedidos` (`CodPed`,`Cod_User`, `Fecha`,`Precio_total`, `Cod_Estado`) VALUES
-	(3, '2022-11-27 19:23:14', 0, 2),
-	(4, '2022-11-27 19:24:17', 0, 2),
-	(5, '2022-11-27 19:25:39', 0, 2);
-
-CREATE TABLE IF NOT EXISTS `pedidosproductos` (
-  `CodPredProd` int(11) NOT NULL AUTO_INCREMENT,
-  `CodPed` int(11) NOT NULL,
-  `CodProd` int(11) NOT NULL,
-  `Unidades` int(11) NOT NULL,
-  `Precio` 
-  PRIMARY KEY (`CodPredProd`),
-  KEY `CodPed` (`CodPed`),
-  KEY `CodProd` (`CodProd`),
-  CONSTRAINT `pedidosproductos_ibfk_1` FOREIGN KEY (`CodPed`) REFERENCES `pedidos` (`CodPed`),
-  CONSTRAINT `pedidosproductos_ibfk_2` FOREIGN KEY (`CodProd`) REFERENCES `productos` (`CodProd`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
-
-INSERT INTO `pedidosproductos` (`CodPredProd`, `CodPed`, `CodProd`, `Unidades`) VALUES
-	(1, 3, 5, 2),
-	(2, 3, 4, 2),
-	(3, 4, 5, 2),
-	(4, 4, 4, 2),
-	(5, 5, 5, 2),
-	(6, 5, 4, 2),
-	(7, 5, 1, 1);
-
-CREATE TABLE IF NOT EXISTS `productos` (
-  `CodProd` int(11) NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(45) DEFAULT NULL,
-  `Descripcion` varchar(90) NOT NULL,
-  `Peso` float NOT NULL,
-  `Stock` int(11) NOT NULL,
-  `CodCat` int(11) NOT NULL,
-  `Activo` boolean NOT NULL,
-  `Precio` float NOT NULL,
-  PRIMARY KEY (`CodProd`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-
-INSERT INTO `productos` (`CodProd`, `Nombre`, `Peso`, `Stock`, `CodCat`,`Estado`, `Precio`) VALUES
+  INSERT INTO `productos` (`CodProd`, `Nombre`, `Peso`, `Stock`, `CodCat`,`Estado`, `Precio`) VALUES
 	(1, 'Corona individual', 8, 100, 1,true,15.4)
 	(2, 'Restauracion multiple atronillada y cementada',10 ,200 ,1 ,true,30),
 	(3, 'Hibridas en resina sobre estructura colada y fresada',25,100 ,1,true ,27),
@@ -89,45 +51,76 @@ INSERT INTO `productos` (`CodProd`, `Nombre`, `Peso`, `Stock`, `CodCat`,`Estado`
   (19,'Combinada: con ataches en combinacion con elementos fijos', , , , , ,)
   (20,'En resina acrilica', , , , , ,)
 
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `Cod_User` int(11) NOT NULL,
-  `Correo` varchar(90) NOT NULL,
-  `Clave` varchar(45) NOT NULL,
-  `Pais` varchar(45) NOT NULL,
-  `Localidad` varchar(45) NOT NULL,
-  `Direccion` varchar(200) NOT NULL,
-  `Cod_Rol`
-  PRIMARY KEY (`CodUser`),
-  UNIQUE KEY `UN_RES_COR` (`Correo`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+-- Tabla "rol"
+CREATE TABLE rol (
+    Cod_Rol INT PRIMARY KEY,
+    Descrip VARCHAR(255)
+  );
 
+  INSERT INTO `rol` (`CodRol`, `Descripcion`,) VALUES
+	(1,  'admin'),
+	(2, 'xefe');
+	(3, 'cliente');
 
-INSERT INTO `usuarios` (`CodRes`, `Correo`, `Clave`, `Pais`, `CP`, `Ciudad`, `Direccion`, `Rol`) VALUES
-	(1, 'sam', '1234', 'España', 15150, 'Baio', 'Bispo Romero', 'admin'),
-	(2, '', '', '', , '', ' ');
+-- Tabla "estado"
+CREATE TABLE estado (
+    Cod_Estado INT PRIMARY KEY,
+    Tipo VARCHAR(255)
+  );  
 
-CREATE TABLE IF NOT EXISTS `rol` (
-  `CodRol` int(11) NOT NULL AUTO_INCREMENT,
-  `Descripcion` varchar(45) NOT NULL,
-  PRIMARY KEY (`CodRol`),
-  UNIQUE KEY `UN_RES_COR` (`Correo`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+-- Tabla "usuarios"
+CREATE TABLE usuarios (
+    Cod_User INT PRIMARY KEY,
+    Correo VARCHAR(255),
+    Clave VARCHAR(255),
+    Direccion VARCHAR(255),
+    Cod_Rol INT,
+    FOREIGN KEY (Cod_Rol) REFERENCES rol(Cod_Rol)
+  );
 
-INSERT INTO `rol` (`CodRol`, `Tipo`,) VALUES
-	(1, 'sam', 'admin'),
-	(2, '', '', '', , '', ' ');
+  INSERT INTO `usuarios` (`CodRes`, `Correo`, `Clave`, `Pais`, `CP`, `Ciudad`, `Direccion`, `Rol`) VALUES
+	(1, 'sam', '1234', 'España', 15150, 'Baio', 'Bispo Romero', 1),
+	(2, 'maria', 'abc123.', 'España',15129 , 'Zas', 'Vilachan ',2);
 
+-- Tabla "pedidos"
+CREATE TABLE pedidos (
+    Cod_Pedido INT PRIMARY KEY,
+    Fecha DATE,
+    Precio_total DECIMAL(10, 2),
+    Cod_User INT,
+    Cod_Estado INT,
+    FOREIGN KEY (Cod_User) REFERENCES usuarios(Cod_User),
+    FOREIGN KEY (Cod_Estado) REFERENCES estado(Cod_Estado)
+  );
+  INSERT INTO `pedidos` (`CodPed`,`Cod_User`, `Fecha`,`Precio_total`, `Cod_Estado`) VALUES
+	(3, '2022-11-27 19:23:14', 0, 2),
+	(4, '2022-11-27 19:24:17', 0, 2),
+	(5, '2022-11-27 19:25:39', 0, 2);  
 
-CREATE TABLE IF NOT EXISTS `estado` (
-  `Cod_Estado` int(11) NOT NULL AUTO_INCREMENT,
-  `Descripcion` varchar(45) NOT NULL,
-  PRIMARY KEY (`Cod_Estado`),
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+-- Tabla "productospedidos"
+CREATE TABLE productospedidos (
+    Cod_ProdPed INT PRIMARY KEY,
+    Unidades INT,
+    Precio DECIMAL(10, 2),
+    Cod_Pedido INT,
+    Cod_Producto INT,
+    FOREIGN KEY (Cod_Pedido) REFERENCES pedidos(Cod_Pedido),
+    FOREIGN KEY (Cod_Producto) REFERENCES productos(Cod_Producto)
+  );
 
+  INSERT INTO `pedidosproductos` (`CodPredProd`, `CodPed`, `CodProd`, `Unidades`) VALUES
+	(1, 3, 5, 2),
+	(2, 3, 4, 2),
+	(3, 4, 5, 2),
+	(4, 4, 4, 2),
+	(5, 5, 5, 2),
+	(6, 5, 4, 2),
+	(7, 5, 1, 1);
 
-
-  CREATE TABLE IF NOT EXISTS `log` (
-  `Cod_User` int(11) NOT NULL AUTO_INCREMENT,
-  `Descripcion` varchar(45) NOT NULL,
-  PRIMARY KEY (`Cod_User`),
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+-- Tabla "log"
+CREATE TABLE log (
+    Cod_Log INT PRIMARY KEY,
+    Cod_User INT,
+    Detalles VARCHAR(255),
+    FOREIGN KEY (Cod_User) REFERENCES usuarios(Cod_User)
+  );
